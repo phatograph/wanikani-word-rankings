0551786b150bd7cf21c21249b553251d6d46b538
Pat Wangrungarun <phatograph@gmail.com>
2017-07-25 11:53:27 +0900

    Save to db

--- crawler.js
+++ crawler.js
@@ -1,18 +1,50 @@
+const axios = require('axios');
+const mongo = require('mongodb').MongoClient;
+const _ = require('lodash');
+
 const crawler = async () => {
-  const axios = require('axios');
-  const mongo = require('mongodb').MongoClient;
+  const db = await mongo.connect('mongodb://localhost:27017/wanikani-word-rankings');
+  let vocabs = [];
+  let counter = 1;
+  let promises = [];
+  const highestLevel = 31;
 
   try {
-    const url = `https://www.wanikani.com/api/user/${process.env.TOKEN}/vocabulary/1`;
-    const results = await axios({
-      url,
-      timeout: 30000,
-    });
+    while (counter <= highestLevel) {
+      promises.push(getVocabs(counter));
+      counter++;
+    }
 
-    console.log(results.data.requested_information.length);
-  } catch (e) {
+    vocabs = vocabs.concat(await Promise.all(promises))
+    vocabs = _.flatten(vocabs);
+
+    await db.collection('vocabularies').remove({});
+    await db.collection('vocabularies').insertMany(vocabs);
+  }
+  catch (e) {
     console.log(e);
   }
+  finally {
+    if (db) {
+      await db.close();
+    }
+  }
+}
+
+const getVocabs = async (level) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const results = await axios({
+        url: `https://www.wanikani.com/api/user/${process.env.TOKEN}/vocabulary/${level}`,
+        timeout: 10000,
+      })
+
+      console.log(results.data.requested_information.length)
+      resolve(results.data.requested_information);
+    } catch (e) {
+      resolve([])
+    }
+  })
 }
 
 crawler();

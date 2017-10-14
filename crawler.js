6528e98fcf032aaf6dda7899fc5225895ed995ee
Pat Wangrungarun <phatograph@gmail.com>
2017-10-14 12:48:26 +0900

    Update crawler.

--- crawler.js
+++ crawler.js
@@ -56,4 +56,62 @@ const getVocabs = async (level) => {
   })
 }
 
-crawler();
+const crawler2 = async () => {
+  let db;
+
+  try {
+    db = await mongo.connect('mongodb://localhost:27017/wanikani-word-rankings-v2');
+    const { data } = await axios({
+      url: `https://www.wanikani.com/api/v2/subjects`,
+      timeout: 10000,
+      headers: {
+        'Authorization': 'Token token=88abab1f-8a95-48b0-b1de-c1cd78b477c7',
+      },
+    });
+
+    const totalPages = data.pages.last;
+    let counter = 1;
+    let promises = [];
+
+    while (counter <= totalPages) {
+      promises = promises.concat(getSubjects(counter));
+      counter++;
+    }
+
+    let subjects = await Promise.all(promises);
+    subjects = _.flatten(subjects);
+
+    await db.collection('vocabularies').remove({});
+    await db.collection('vocabularies').insertMany(subjects);
+    console.error(`Saved ${subjects.length}`)
+  }
+  catch (e) {
+    console.log(e)
+  }
+  finally {
+    if (db) {
+      await db.close();
+    }
+  }
+}
+
+const getSubjects = async (level) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { data } = await axios({
+        url: `https://www.wanikani.com/api/v2/subjects?page=${level}`,
+        timeout: 10000,
+        headers: {
+          'Authorization': 'Token token={88abab1f-8a95-48b0-b1de-c1cd78b477c7}',
+        },
+      })
+
+      console.log(data.data.length)
+      resolve(data.data);
+    } catch (e) {
+      resolve([])
+    }
+  })
+}
+
+crawler2();
